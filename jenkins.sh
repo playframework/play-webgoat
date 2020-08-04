2e19ed477a51dfac69a49e7990396854bf96fe43
Seth Tisue <seth@tisue.net>
2020-08-04 11:59:52 -0700

    SCA 20.1

--- jenkins.sh
+++ jenkins.sh
@@ -14,7 +14,7 @@ else
    SCA_DIR=/home/ubuntu/Fortify
    SBT=/usr/bin/sbt
 fi
-SOURCEANALYZER=$SCA_DIR/Fortify_SCA_and_Apps_19.2.3/bin/sourceanalyzer
+SOURCEANALYZER=$SCA_DIR/Fortify_SCA_and_Apps_20.1.2/bin/sourceanalyzer
 $SOURCEANALYZER -b play-webgoat -clean
 
 java -version
